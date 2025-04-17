18107afb03d02fa13657b1bcff91abcc056a3af6
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2025-04-17 13:59:36 +0800

    Update setup_postgresql.sh

--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -5,4 +5,3 @@ echo | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
 echo | sudo apt install postgresql-15-pgvector
 sudo service postgresql start
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'password';"
-sudo -u postgres psql -c "CREATE DATABASE vector_db;"
